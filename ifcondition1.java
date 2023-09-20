680f272812e2dad0576620694c398d362d4a92ee
Tanvir Alam Mugdha <115610092+tanviralammugdha@users.noreply.github.com>
2023-09-20 11:04:18 +0600

    Create ifcondition1.java

---
+++ ifcondition1.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication6;
+
+/**
+ *
+ * @author MY PC
+ */
+public class JavaApplication6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int age=30;
+        if(age>25){
+        System.out.print("age is gater than 18 ");
+        }
+        
+    }
+    
+}
