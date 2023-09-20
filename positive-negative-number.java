46e31db88535734f29673bd19e44ee38eecd1b6a
Tanvir Alam Mugdha <115610092+tanviralammugdha@users.noreply.github.com>
2023-09-20 11:24:56 +0600

    Create positive-negative-number.java

---
+++ positive-negative-number.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication6;
+
+/**
+ *
+ * @author MY PC
+ */
+public class JavaApplication6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       
+     
+     int a=1;
+     if (a>0){
+     System.out.print("is positive number");
+     }
+     else if (a<0){
+     System.out.print("is negative number");
+     
+     }
+     else{
+     System.out.print("this is zero");
+     }
+        
+    
+            }
+    
+}
